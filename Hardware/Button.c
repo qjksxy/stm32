0f324c14272ca62e2f092b93baaaa94cce27b82d
Apin <81305669+qjksxy@users.noreply.github.com>
2024-03-05 07:17:43 +0800

    :sparkles: - 新增至支持三个按键，占用 A3，A5，A7 引脚 - 当按钮按下时，OLED屏幕右下角会显示按钮序号

--- Hardware/Button.c
+++ Hardware/Button.c
@@ -1,12 +1,13 @@
 #include "stm32f10x.h"                  // Device header
 #include "delay.h"
+#include "OLED.h"
 
-
+// 引脚初始化
 void Button_Init(void) {
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_5 | GPIO_Pin_7;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOA, &GPIO_InitStructure);
 }
@@ -14,17 +15,26 @@ void Button_Init(void) {
 // 按键按下检测
 uint8_t Button_GetNum(void) {
     uint8_t ButtonNum = 0;
+    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_3) == 0) {
+        Delay_ms(20);
+        while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_3) == 0);
+        Delay_ms(20);
+        ButtonNum = 1;
+    }
     if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5) == 0) {
         Delay_ms(20);
         while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5) == 0);
         Delay_ms(20);
-        ButtonNum = 1;
+        ButtonNum = 2;
     }
     if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 0) {
         Delay_ms(20);
         while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 0);
         Delay_ms(20);
-        ButtonNum = 2;
+        ButtonNum = 3;
+    }
+    if(ButtonNum != 0) {
+        OLED_Show_Int_Info(ButtonNum);
     }
     return ButtonNum;
 }
