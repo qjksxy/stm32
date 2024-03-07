de8a3d7db415f037c031a4496b6926381233913a
Apin <81305669+qjksxy@users.noreply.github.com>
2024-03-07 10:39:16 +0800

    :art: 解耦引脚定义

--- Hardware/Button.c
+++ Hardware/Button.c
@@ -1,35 +1,41 @@
 #include "stm32f10x.h"                  // Device header
 #include "delay.h"
 #include "OLED.h"
+#define BUTTON_RCC_APB2Periph    RCC_APB2Periph_GPIOA
+#define BUTTON_1_GPIO_PIN        GPIO_Pin_3
+#define BUTTON_2_GPIO_PIN        GPIO_Pin_5
+#define BUTTON_3_GPIO_PIN        GPIO_Pin_7
+#define BUTTON_GPIOx             GPIOA
+
 
 // 引脚初始化
 void Button_Init(void) {
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    RCC_APB2PeriphClockCmd(BUTTON_RCC_APB2Periph, ENABLE);
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_3 | GPIO_Pin_5 | GPIO_Pin_7;
+    GPIO_InitStructure.GPIO_Pin = BUTTON_1_GPIO_PIN | BUTTON_2_GPIO_PIN | BUTTON_3_GPIO_PIN;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_Init(BUTTON_GPIOx, &GPIO_InitStructure);
 }
 
 // 按键按下检测
 uint8_t Button_GetNum(void) {
     uint8_t ButtonNum = 0;
-    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_3) == 0) {
+    if (GPIO_ReadInputDataBit(BUTTON_GPIOx, BUTTON_1_GPIO_PIN) == 0) {
         Delay_ms(20);
-        while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_3) == 0);
+        while(GPIO_ReadInputDataBit(BUTTON_GPIOx, BUTTON_1_GPIO_PIN) == 0);
         Delay_ms(20);
         ButtonNum = 1;
     }
-    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5) == 0) {
+    if (GPIO_ReadInputDataBit(BUTTON_GPIOx, BUTTON_2_GPIO_PIN) == 0) {
         Delay_ms(20);
-        while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_5) == 0);
+        while(GPIO_ReadInputDataBit(BUTTON_GPIOx, BUTTON_2_GPIO_PIN) == 0);
         Delay_ms(20);
         ButtonNum = 2;
     }
-    if (GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 0) {
+    if (GPIO_ReadInputDataBit(BUTTON_GPIOx, BUTTON_3_GPIO_PIN) == 0) {
         Delay_ms(20);
-        while(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_7) == 0);
+        while(GPIO_ReadInputDataBit(BUTTON_GPIOx, BUTTON_3_GPIO_PIN) == 0);
         Delay_ms(20);
         ButtonNum = 3;
     }
