969f34aa277d6091e599274fda17d9fe3e3aa19d
Apin <81305669+qjksxy@users.noreply.github.com>
2024-03-07 15:29:59 +0800

    :art: 解耦引脚

--- Hardware/LED.c
+++ Hardware/LED.c
@@ -1,33 +1,36 @@
 #include "stm32f10x.h"                  // Device header
-
+#define LED_RCC_APB2Periph RCC_APB2Periph_GPIOA
+#define LED_0_GPIO_PIN GPIO_Pin_0
+#define LED_1_GPIO_PIN GPIO_Pin_1
+#define LED_GPIOx GPIOA
 
 void LED_Init_01(void) {
-    // 时钟使能
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+    // 时钟使能 
+    RCC_APB2PeriphClockCmd(LED_RCC_APB2Periph, ENABLE);
     // 定义结构体，设置端口输入输出模式
     GPIO_InitTypeDef GPIO_InitStructure;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_1;
+    GPIO_InitStructure.GPIO_Pin = LED_0_GPIO_PIN | LED_1_GPIO_PIN;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    GPIO_Init(LED_GPIOx, &GPIO_InitStructure);
     
-    GPIO_SetBits(GPIOA, GPIO_Pin_0);
-    GPIO_SetBits(GPIOA, GPIO_Pin_1);
+    GPIO_SetBits(LED_GPIOx, LED_0_GPIO_PIN);
+    GPIO_SetBits(LED_GPIOx, LED_1_GPIO_PIN);
 }
 
 // A0灯亮
 void LED_Green_On(void) {
-    GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+    GPIO_ResetBits(LED_GPIOx, LED_0_GPIO_PIN);
 }
 // A1灯亮
 void LED_Red_On(void) {
-    GPIO_ResetBits(GPIOA, GPIO_Pin_1);
+    GPIO_ResetBits(LED_GPIOx, LED_1_GPIO_PIN);
 }
 // A0灯灭
 void LED_Green_Off(void) {
-    GPIO_SetBits(GPIOA, GPIO_Pin_0);
+    GPIO_SetBits(LED_GPIOx, LED_0_GPIO_PIN);
 }
 // A1灯灭
 void LED_Red_Off(void) {
-    GPIO_SetBits(GPIOA, GPIO_Pin_1);
+    GPIO_SetBits(LED_GPIOx, LED_1_GPIO_PIN);
 }
